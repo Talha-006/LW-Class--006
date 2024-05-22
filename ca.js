39337356c3d20bdd50e4038129f0fa4e6c43ba8f
Talha-006 <163578732+Talha-006@users.noreply.github.com>
2024-05-23 00:06:44 +0400

    Add files via upload

---
+++ ca.js
@@ -0,0 +1,10 @@
+var studetns = ["talha", "momin", "jawad"];
+//let searchstudents = studetns.findIndex(findmomin);
+//studetns [5] = "talha" ;
+// studetns.sort();
+studetns. splice(2,1, ... ["Zahid", "Anwar"]);
+console. log(...studetns);
+function findmomin(std){
+if(std == "'momin") return true;
+else return false;
+}
---
+++ class.js
@@ -0,0 +1,6 @@
+/*let test =() => {
+    return "momin";
+}
+console.log(test());*/           
+let test = ()=> "momin";
+console.log(test());
